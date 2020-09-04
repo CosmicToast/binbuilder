1b523ea7eca5fe652bfbcc9fc63574fd337a9b7d
Chloe Kudryavtsev <toast@toast.cafe>
2020-09-04 16:34:04 -0400

    add install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# this will install everything under ./bin into ~/bin, as if you uploaded it all and used dl.sh
+# if you call it with arguments, it will use the specific files
+# this will fail horribly if there are no files, so good luck lol
+
+[ $# -eq 0 ] && find bin -type f -exec ./$0 {} +
+
+mkdir -p ~/bin
+for i
+do
+	f="${i##*/}"
+	f="${f%%-*}"
+	echo "installing $i to ~/bin/$f"
+	cp "$i" ~/bin/"$f"
+done
