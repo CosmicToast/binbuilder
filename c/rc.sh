3fa1190022856eea51190a48318a7cf1a6b6afb7
Chloe Kudryavtsev <toast@toast.cafe>
2020-10-24 19:29:48 -0400

    link rc against readline

--- c/rc.sh
+++ c/rc.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-apk add bison libedit-dev ncurses-dev
+apk add bison ncurses-dev readline-dev
 # both EDIT and LDLIBS need to be defined
 # EDIT so that the correct .c file is included in OBJS
 # LDLIBS to force adding ncursesw
 # TODO: contribute to the hand-written makefile with the added system LIBS= :)
-make EDIT=edit CC=clang YACC=yacc LDLIBS='-ledit -lncursesw' -j $(nproc)
+make EDIT=readline CC=clang YACC=yacc LDLIBS='-lreadline -lncursesw' -j $(nproc)
 bin=rc
