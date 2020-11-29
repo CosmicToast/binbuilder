a537723b4161f49fda70805f034f0e898117d62e
Chloe Kudryavtsev <toast@toast.cafe>
2020-11-29 11:00:24 -0500

    start working on c.sh options

--- c.sh
+++ c.sh
@@ -7,6 +7,50 @@ cd "$name"
 export CFLAGS='-Os'
 export CXXFLAGS='-static -Os'
 export LDFLAGS='-all-static -static'
+
+# options!
+# - apk deps
+if has_opt "apk_add"; then
+	# TODO: implement
+	# how?
+fi
+
+# - link utilities
+if has_opt "link"; then
+	ln -s clang /bin/cc
+	ln -s clang++ /bin/cxx
+	for i in ar ranlib; do
+		ln -s llvm-$i /bin/$i
+	fi
+fi
+
+# - git submodules
+if has_opt "git_submodules"; then
+	git submodule update --init
+fi
+
+# - libtoolize
+if has_opt "libtoolize"; then
+	touch install.sh
+	libtoolize -cfi
+fi
+
+# - autoreconf
+if has_opt "autoreconf"; then
+	autoreconf -fi
+fi
+
+# - autogen.sh
+if has_opt "autogen.sh"; then
+	./autogen.sh
+fi
+
+# - do a plain configure
+# TODO: configure opts? how?
+if has_opt "configure"; then
+	./configure
+fi
+
 if [ -z "$mod" ]
 then # custom handling
 	. "../c/$name.sh"
