eebe7cbea2acba5a037121cf73b2da41893b9300
Chloe Kudryavtsev <toast@toast.cafe>
2021-12-17 15:23:32 -0500

    go: rework a bunch of stuff, read the patch

--- go.bash
+++ go.bash
@@ -2,16 +2,29 @@
 set -e
 
 # extra accepted flags:
+# * bnames: rename every binary name to this in order
+# * versionvar: var to -X the version into
+# * commitvar: var to -X the commit into
+# * buildvar: var to -X the builddate into
+# * buildervar: var to -X the builder info into
 
 # extra accepted options:
 # * cgo: enable cgo
 # * generate: runs go generate
 # * rename: rename every basename of each module to the corresponding binary name in order
+# * goldflags: additional go ldflags
 
 # defaults
 type=go
+goldflags=(-s -w)
 . ./options.bash
 cd "$pdir"
+
+# goldflags
+[ -v versionvar ] && goldflags+=( -X "$versionvar=$VERSION" )
+[ -v commitvar ]  && goldflags+=( -X "$commitvar=$COMMIT" )
+[ -v buildvar ]   && goldflags+=( -X "$buildvar=$BUILDDATE" )
+[ -v buildervar ] && goldflags+=( -X "$buildervar=$BUILDER" )
 
 # build
 if ! has_opt cgo; then
@@ -26,7 +39,7 @@ fi
 
 for mm in "${mod[@]}"; do
 	if [[ -d $mm ]]; then
-		go build -ldflags='-s -w' "$mm"
+		go build -ldflags="${goldflags[*]}" "$mm"
 	else
 		make "$mm"
 	fi
@@ -37,7 +50,7 @@ done
 if has_opt rename && let "${#bin[@]} == ${#mod[@]}"; then
 	num=$(( ${#bin[@]} - 1 ))
 	for ii in $(seq 0 $num); do
-		mv $(basename ${mod[$ii]}) ${bin[@]}
+		mv $(basename ${mod[$ii]}) ${bin[$ii]}
 	done
 fi
 
@@ -49,6 +62,16 @@ if ! let ${#bin[@]} && let ${#mod[@]}; then
 		bin+=("$bm")
 	done
 fi
+
+# renames round 2
+if let "${#bnames[@]} == ${#bin[@]}"; then
+	num=$(( ${#bin[@]} - 1 ))
+	for ii in $(seq 0 $num); do
+		mv ${bin[$ii]} ${bnames[$ii]}
+		bin[$ii]=${bnames[$ii]}
+	done
+fi
+
 let ${#bin[@]} || declare -n bin=name
 for f in "${bin[@]}"; do handlebin "$f"; done
 clean
